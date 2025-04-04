e0afdeff17a10a8bca98e3c352914850597a34e1
mudasirking <165293870+SahilKing71@users.noreply.github.com>
2025-04-05 00:44:11 +0500

    Create Style.c

---
+++ Style.c
@@ -0,0 +1,25 @@
+body {
+ font-family: Arial, sans-font-family;
+ background-color: #f2f2f2;
+}
+
+h1 {
+ color: #00698f;
+}
+
+p {
+ color: #666;
+}
+
+button {
+ background-color: #4CAF50;
+ color: #fff;
+ padding: 10px 20px;
+ border: none;
+ border-radius: 5px;
+ cursor: pointer;
+}
+
+button:hover {
+ background-color: #3e8e41;
+  }
